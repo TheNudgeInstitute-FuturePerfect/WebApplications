ad7e23dc1aeada19aee2063026e29a331b67efd7
Aashutosh Chaudhary <aashutosh@instasalary.app>
2023-05-21 15:06:59 +0530

    eClass app

--- src/home.js
+++ src/home.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+import eclass from "./images/e-class.png";
+
 const Home = () => {
-  return <></>;
+  return (
+    <div className="row p-5">
+      <div className="col-2 shadow rounded text-center p-4">
+        <h4 className="">eClass</h4>
+        <img src={eclass} alt="eClass" className="w-100" />
+        <Link
+          to={`${process.env.REACT_APP_API_ENDPOINT}/e-class/`}
+          style={{ fontSize: 20 }}
+          className="btn btn-primary text-decoration-none ps-5 pe-5"
+        >
+          Start
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
