c4a9104607c5166ec24c98a40212d052ac4d93e3
Aashutosh Chaudhary <aashutosh@instasalary.app>
2023-05-21 15:08:46 +0530

    fix

--- src/home.js
+++ src/home.js
@@ -4,7 +4,7 @@ import talkNote from "./images/talk-note.png";
 const Home = () => {
   return (
     <div className="row p-5">
-      <div className="col-4 shadow rounded text-center p-4">
+      <div className="col-2 shadow rounded text-center p-4">
         <h4 className="">Talk Note</h4>
         <img src={talkNote} alt="Talk Note" className="w-100" />
         <Link
