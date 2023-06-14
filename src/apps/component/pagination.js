6ab79167fbc0e4717c7f360701e0c1805f487d94
Aashutosh Chaudhary <aashutosh@instasalary.app>
2023-06-14 12:31:10 +0530

    pagination fix

--- src/apps/component/pagination.js
+++ src/apps/component/pagination.js
@@ -1,4 +1,29 @@
 function Pagination({ currentPage, totalPages, totalDocuments, paginate }) {
+  const pages = () => {
+    if (totalPages > 10)
+      if (currentPage > 5 && currentPage < totalPages - 5 + 1)
+        return [
+          ...Array.from(
+            { length: 5 },
+            (value, index) => index + currentPage - 5
+          ),
+          "...",
+          ...Array.from(
+            { length: 5 },
+            (value, index) => index + totalPages - 5
+          ),
+        ];
+      else
+        return [
+          ...Array.from({ length: 5 }, (value, index) => index),
+          "...",
+          ...Array.from(
+            { length: 5 },
+            (value, index) => index + totalPages - 5
+          ),
+        ];
+    else return Array.from({ length: totalPages }, (value, index) => index);
+  };
   return (
     <div className="mt-1 text-center">
       <div
@@ -17,23 +42,25 @@ function Pagination({ currentPage, totalPages, totalDocuments, paginate }) {
       >
         Previous
       </div>
-      {Array.from(Array(totalPages || currentPage).keys()).map(
-        (element, index) => (
-          <div
-            key={index}
-            className={`d-inline-block border p-2 me-1 mt-1 cursor-pointer${
-              currentPage === element + 1
-                ? " bg-primary text-light"
-                : " text-primary"
-            }`}
-            onClick={() =>
-              currentPage === element + 1 ? null : paginate(element + 1)
-            }
-          >
-            {element + 1}
-          </div>
-        )
-      )}
+      {pages().map((element, index) => (
+        <div
+          key={index}
+          className={`d-inline-block border p-2 me-1 mt-1 cursor-pointer${
+            currentPage === element + 1
+              ? " bg-primary text-light"
+              : " text-primary"
+          }`}
+          onClick={() =>
+            currentPage === element + 1
+              ? null
+              : element === "..."
+              ? null
+              : paginate(element + 1)
+          }
+        >
+          {element === "..." ? element : element + 1}
+        </div>
+      ))}
       <div
         className={`d-inline-block border p-2 me-1 mt-1 cursor-pointer${
           currentPage === totalPages ? " text-secondary" : " text-primary"
