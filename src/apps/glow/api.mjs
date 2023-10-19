667b8b4ffcd1a1aa416f8b10bd61629da91170a8
Aashutosh Chaudhary <aashutosh.chaudhary@thenudge.org>
2023-10-19 17:48:11 +0530

    nosql

--- src/apps/glow/api.mjs
+++ src/apps/glow/api.mjs
@@ -4,7 +4,7 @@ const getSessionData = async (SessionID) => {
   if (SessionID) {
     try {
       const response = await axios.get(
-        `${process.env.REACT_APP_API_ENDPOINT}/api/glow/feedback?action=list&table=Sessions&condition=SessionID='${SessionID}' AND MessageType='UserMessage' ORDER BY CREATEDTIME`
+        `${process.env.REACT_APP_API_ENDPOINT}/api/glow/feedback?collection=sessions&SessionID='${SessionID}'`
       );
       if (response.data.data instanceof Array && response.data.data.length)
         trackLink(
